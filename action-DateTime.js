a1dec5dbdaf0c3f076701271602716b7878bea12
Snips-RS-User <49959746+Snips-RS-User@users.noreply.github.com>
2019-04-28 14:02:41 +0200

    Add files via upload

--- action-DateTime.js
+++ action-DateTime.js
@@ -1,5 +1,57 @@
 #!/usr/bin/env node
 
+var mqtt = require('mqtt');
+
+var hostname = "localhost";
+var client  = mqtt.connect('mqtt://' + hostname, { port: 1883 });
+
+client.on('connect', function () {
+    console.log("[Snips Log] Connected to MQTT broker " + hostname);
+    client.subscribe('hermes/#');
+});
+
+client.on('message', function (topic, payload) {
+    if (topic === "hermes/asr/startListening") {
+        onListeningStateChanged("Start");
+    } else if (topic === "hermes/asr/stopListening") {
+        onListeningStateChanged("Stop");
+    } else if (topic.match(/hermes\/hotword\/.+\/detected/g) !== null) {
+        onHotwordDetected()
+    } else if (topic.match(/hermes\/intent\/.+/g) !== null) {
+        onIntentDetected(JSON.parse(payload));
+    }
+});
+
+function onListeningStateChanged(listening) {
+    console.log("[Snips Log] " + listening + " listening");
+}
+
+function onHotwordDetected() {
+    console.log("[Snips Log] Hotword detected");
+}
+
+function onIntentDetected(payload) {
+    var sessionId = payload["sessionId"];
+    var siteId = payload["siteId"];
+    var intentName = payload["intent"]["intentName"];
+    var confidenceScore = payload["intent"]["confidenceScore"];
+    var slots = payload["slots"];
+    console.log("[Snips Log] Intent detected: sessionId="+ sessionId + " - siteId=" + siteId);
+    console.log("[Snips Log] Intent detected: IntentName="+ intentName + " - Slots=" + JSON.stringify(slots) + " - confidenceScore=" + confidenceScore);
+    if (intentName === "Snips-RS-User:test1") {
+
+    }
+    if (intentName === "Snips-RS-User:test2") {
+
+    }
+    if (intentName === "Snips-RS-User:test3") {
+
+    }
+}
+
+
+
+/*
 const ini = require('ini')
 const fs = require('fs')
 
@@ -30,7 +82,7 @@ withHermes((hermes, done) => {
     }
 })
 
-
+*/
 
 
 
