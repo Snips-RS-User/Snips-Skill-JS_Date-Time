5b6eedaaa41e00e6b379c64caa60abd2df27f896
Snips-RS-User <compte-rs.snips@outlook.fr>
2019-05-14 00:24:38 +0200

    ADD config.ini file and update setup.sh for the parameters

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #/usr/bin/env bash -e
 
+DEFAULT_CONFIG_FILE="./config.ini.default"
+CONFIG_FILE="./config.ini"
+
+
+# user config version checking
+if [ ! -e $CONFIG_FILE ]
+then
+    cp config.ini.default config.ini
+fi
+
 npm install 
