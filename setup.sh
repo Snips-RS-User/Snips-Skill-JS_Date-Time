4f88717f18867a91eb376496fb5e3ef2a60cd7cc
Snips-RS-User <compte-rs.snips@outlook.fr>
2019-05-31 23:02:32 +0200

    Version 1.0.2 (executable version)

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #/usr/bin/env bash -e
 
-DEFAULT_CONFIG_FILE="./config.ini.default"
-CONFIG_FILE="./config.ini"
-
+#DEFAULT_CONFIG_FILE="./config.ini.default"
+#CONFIG_FILE="./config.ini"
 
 # user config version checking
-if [ ! -e $CONFIG_FILE ]
-then
-    cp config.ini.default config.ini
-fi
+#if [ ! -e $CONFIG_FILE ]
+#then
+#    cp config.ini.default config.ini
+#fi
+
 
 npm install 
+chmod +x action-DateTime.js
