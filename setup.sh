5e0299b298329795bf08fcf1a282c01735ac3168
Snips-RS-User <compte-rs.snips@outlook.fr>
2019-06-01 23:54:53 +0200

    Fix start the program automatically

--- setup.sh
+++ setup.sh
@@ -1,14 +1,4 @@
 #/usr/bin/env bash -e
 
-#DEFAULT_CONFIG_FILE="./config.ini.default"
-#CONFIG_FILE="./config.ini"
-
-# user config version checking
-#if [ ! -e $CONFIG_FILE ]
-#then
-#    cp config.ini.default config.ini
-#fi
-
-
 npm install 
 chmod +x action-DateTime.js
